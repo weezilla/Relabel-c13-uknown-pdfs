568dbe88c4047811a48968f9a05882ee125a1725
weezilla <weezilla@gmail.com>
2016-01-25 02:27:52 -0500

    started adding excel reading functionality

--- exportUnknownPDFv1.py
+++ exportUnknownPDFv1.py
@@ -1,4 +1,6 @@
 import PyPDF2
+import pyexcel as pe
+import pyexcel.ext.xlsx
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.units import inch
@@ -14,7 +16,22 @@ pagesToPull = sys.argv[2:]
 
 packet = StringIO.StringIO()
 can = canvas.Canvas(packet,pagesize=letter)
-def drawOverlay(aCanvas):
+
+unknownList = pe.get_sheet(file_name=dirPath + "unknowns2016spring.xlsx")
+#print(unknownList[3,1])
+index=0
+while unknownList[index,1]:
+	print unknownList[index,1]
+	drawOverlay(can,unknownList[index,1],unknownList[index,0])
+	
+	index=index+1
+
+
+#Takes sample name and excel sheet and returns the unknown number
+def resolveUnknownNumber():
+	pass
+
+def drawOverlay(aCanvas,fileName,unknownNumber):
 	aCanvas.setFont('Helvetica',14)
 	aCanvas.setFillColorRGB(1,1,1)
 	aCanvas.setStrokeColorRGB(0,0,0)
@@ -22,7 +39,7 @@ def drawOverlay(aCanvas):
 	aCanvas.rotate(90)
 	aCanvas.setFillColorRGB(0,0,0)
 	aCanvas.setStrokeColorRGB(0,0,0)
-	aCanvas.drawString(1*inch,1*-inch, "link to unknown input from excel list")
+	aCanvas.drawString(1*inch,1*-inch, unknownNumber)
 	aCanvas.showPage()
 	aCanvas.save()
 	packet.seek(0)
@@ -44,3 +61,8 @@ pyObjPDF.close()
 
 #https://automatetheboringstuff.com/chapter13/
 #https://www.binpress.com/tutorial/manipulating-pdfs-with-python/171
+#https://www.reportlab.com/docs/reportlab-userguide.pdf
+#http://stackoverflow.com/questions/1180115/add-text-to-existing-pdf-using-python
+#http://www.tylerlesmann.com/2009/jan/28/writing-pdfs-python-adding-images/
+#https://www.binpress.com/tutorial/manipulating-pdfs-with-python/167
+#
